f444db5186ae36e53d12b6b4af021687331bff69
MADHAN-KUMAR-V <madhankumar@myyahoo.com>
2024-11-02 20:36:05 +0530

    loading add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loader from "./components/utils/Loading";
 
 // Lazy loading components
 const Home = lazy(() => import("./pages/Home"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
-const SideNavLayout = lazy(() => import("./components/Side Navbar/SideNavLayout"));
+const SideNavLayout = lazy(() =>
+  import("./components/Side Navbar/SideNavLayout")
+);
 const Signal = lazy(() => import("./pages/Signal"));
-const BigInvestor = lazy(() => import("./pages/BigInvester")); 
+const BigInvestor = lazy(() => import("./pages/BigInvester"));
 const BulkDeals = lazy(() => import("./pages/BulkDeals"));
-const History = lazy(() => import("./pages/History")); 
+const History = lazy(() => import("./pages/History"));
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+   
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); 
+
+    
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />; 
+  }
+
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<SideNavLayout />}>
-            <Route index element={<Dashboard />} /> 
-            <Route path="signal" element={<Signal />} />  
-            <Route path="big-investor" element={<BigInvestor />} /> 
-            <Route path="bulk-deals" element={<BulkDeals />} /> 
-            <Route path="history" element={<History />} />            
+            <Route index element={<Dashboard />} />
+            <Route path="signal" element={<Signal />} />
+            <Route path="big-investor" element={<BigInvestor />} />
+            <Route path="bulk-deals" element={<BulkDeals />} />
+            <Route path="history" element={<History />} />
           </Route>
         </Routes>
       </Suspense>
